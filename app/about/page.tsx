2473d2cf54079d23e8ad393af6be06b27b5911d1
Nakdimon <gjherrera.i@gmail.com>
2024-12-22 21:52:09 +0800

    Short NextJs14 tutorial

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        {" "}
+        This is about page. Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Minima, eius doloremque iure ullam necessitatibus esse? Molestias
+        atque voluptas unde aspernatur iusto maiores consequuntur temporibus rem
+        eaque. Est inventore itaque fuga.
+      </p>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <header className="site-header">
+          <h3>Our Header</h3>
+        </header>
+        {children}
+        <footer className="site-footer">
+          <p>Our footer</p>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About us</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </nav>
+        </footer>
+      </body>
+    </html>
+  );
+}
